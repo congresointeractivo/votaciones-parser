5958c63865f8a7a22b7ded99ec8b71c83f8f091b
Martín Szyszlican <martinsz@gmail.com>
2013-05-19 03:33:57 -0300

    Agregando get_votaciones.py (desde https://github.com/jcarizza/descargar-votaciones) con correcciones para nombres de archivo mal escritos y formatos raros.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+Este repositorio contiene las actas de votaciones nominales del congreso de la nación. Detalle:
+* Actas en PDF de texto desde el año 2001 hasta mayo del 2013.
+* Actas en PDF de imágen (necesitan OCR) de los años 1999 y 2000.
+* CSV extraídos de las actias usando votaciones-parser (algunos archivos fallaron 17/may/13)
+
+parse_all_votaciones.sh
+=======================
+
+Es un shell script que invoca a parse_votaciones.py para todos los PDFs de un directorio
+
+## Uso
+
+Ejemplo
+
+	$ ./parse_all_votaciones actas_2001-may2013 csv
+
 votaciones-parser
 =================
 
@@ -35,4 +51,51 @@ Para ver las opciones de uso:
 ## License
 
 Junto con este proyecto, deberías recibir un archivo denominado ``LICENSE`` con los términos de la licencia.
+
+
+
+Descargar votaciones
+====================
+
+Descargar PDFs de votaciones nominales.
+
+## Requisitos
+
+    Python (>= 2.7)
+    BeautifulSoup (4.1.3)
+
+
+## Como usar
+
+Ejemplo:
+
+    $ python get_votaciones.py --url http://www.diputados.gov.ar/secadmin/ds_electronicos/periodo/2013/index.html --folder votaciones_2013
+
+    Otras URL de ejemplo:
+    http://www.diputados.gov.ar/secadmin/ds_electronicos/periodo/2012/index.html
+    http://www.diputados.gov.ar/secadmin/ds_electronicos/periodo/2009/index.html
+    etc.
+
+Para ver las opciones de uso:
+
+    $ python  get_votaciones.py --help
+
+    Descargar PDFs de votaciones nominales
+
+    optional arguments:
+      -h, --help       show this help message and exit
+      --folder FOLDER  Carpeta destino para PDF descargados
+      --keep-links     Conservar links de PDF en un archivo llamado links.txt
+      --url URL        URL del período de votaciones nominales Ej.: http://www.di
+                       putados.gov.ar/secadmin/ds_electronicos/periodo/2013/index.
+                       html
+
+## License
+
+Junto con este proyecto, deberías recibir un archivo denominado ``LICENSE`` con los términos de la licencia.
+
+
+
+
+
 
---
+++ get_votaciones.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+# encoding: utf8
+
+import urllib2
+import urllib
+import re
+import os
+import sys
+import argparse
+from bs4 import BeautifulSoup
+
+
+def check_url(url):
+    if not (url.startswith("http://") or url.startswith("https://")):
+        log("\nURL need http:// or https:// prefix\n")
+        sys.exit(1)
+
+
+def create_folder(folder):
+    """ Create folder if not exists """
+    if os.path.exists(folder) is False:
+        os.mkdir(folder)
+
+    return folder
+
+
+def save_links(links):
+    """ Save links after download """
+    with open("links.txt", "w") as f:
+        for link in links:
+            f.write("%s\n" % (link))
+        f.close()
+    log("\nLinks saved on %s file\n" % (f.name))
+
+
+def log(data):
+    """ print some info """
+    sys.stdout.write(data)
+    sys.stdout.flush()
+
+
+def get_links(url):
+    """ get pdf download links """
+    log("Fetching links...")
+
+    response = urllib2.urlopen(url)
+    _html = BeautifulSoup(response.read())
+
+    # Get links
+    links = []
+    for a in _html.find(class_="treeview").find_all("a", href=re.compile("\s*.pdf")):
+        links.append(a.attrs["href"])
+
+    log("\nPDF links found: %i\n" % (len(links)))
+    return links
+
+
+def download(links, output_folder):
+    """ Download each one pdf file """
+
+    total = len(links)
+    downloaded = 0
+    for pdf_url in links:
+
+	#Chanchada que corrige algunas URLs mal escritas en las páginas del congreso
+	if pdf_url.startswith("/secadmin/ds_electronicos/periodo/"):
+                log("\nURL problematica corregida "+pdf_url)
+		pdf_url = "http://"+pdf_url[39:len(pdf_url)]
+
+	#Chanchada hardcodeando correcciones a los errores de tipeo en las páginas del congreso
+	if pdf_url == "http://www1.hcdn.gov.ar/dependencias/dselectronicos/actas/2011/129OT01_01_R1.pdf":
+		log("\nURL problematica reemplazada por versión correcta"+pdf_url)
+		pdf_url = "http://www1.hcdn.gov.ar/dependencias/dselectronicos/actas/2011/129OT01_02_R01.pdf"
+
+        file_name = os.path.basename(pdf_url)
+
+	try:
+		# Acomodar el string para que se banque caracteres unicode
+		pdf_url = urllib.quote(pdf_url.strip().encode('utf8'),":/")
+	        urllib.urlretrieve(pdf_url, "%s/%s" % (output_folder, file_name))
+	except IOError, error:
+		log("Error downloading "+pdf_url+","+output_folder+", "+file_name)
+
+        # Update status
+        downloaded += 1
+        log("\rDownloaded: %d/%d" % (downloaded, total))
+
+    log("\nFinalized...")
+
+
+# Getting arguments
+parser = argparse.ArgumentParser(description='Descargar PDF de votaciones nominales')
+parser.add_argument("--folder",
+                    help="Carpeta destino para PDF descargados",
+                    type=create_folder, required=True)
+
+parser.add_argument('--keep-links',
+                    action='store_true',
+                    help='Conservar links de PDF en un archivo links.txt')
+
+parser.add_argument("--url",
+                    help="URL del período de \
+                    votaciones nominales Ej.: \
+                    http://www.diputados.gov.ar/secadmin/ds_electronicos/periodo/2013/index.html",
+                    required=True)
+
+
+if __name__ == "__main__":
+
+    # Get args
+    args = parser.parse_args()
+    output_folder = args.folder
+    url = args.url
+    keep_links = args.keep_links
+
+    # Start process
+    check_url(url)
+    links = get_links(url)
+    download(links, output_folder)
+
+    # Keep links?
+    if keep_links:
+        save_links(links)

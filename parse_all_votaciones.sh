7fde48792b3cab5b28d3b5bbc5b6f220b013b769
Martín Szyszlican <martinsz@gmail.com>
2013-05-19 03:35:28 -0300

    Agregando bash script para parsear todas las votaciones juntas

---
+++ parse_all_votaciones.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ ! "$1" ]; then
+  echo "El primer parámetro debe ser la carpeta en la que están los PDFs"
+  exit
+fi 
+if [ ! "$2" ]; then
+  echo "El segundo parámetro debe ser la carpeta en la que se guardaran los archivos procesados"
+  exit
+fi 
+
+INDIR="$1"
+OUTDIR="$2"
+
+for file in `ls "$INDIR"`
+do
+	echo "Procesando $INDIR/$file..."
+	python parse_votaciones.py "$INDIR/$file" "$OUTDIR/$file.csv"
+done
